d9da067a2527cabc48e10ec14e4d07c7d22c0a97
Bryce Wray <bwray@wraytx.com>
2021-03-25 09:58:16 -0500

    NO pagination (d'oh)

--- src/_includes/layouts/_default/index.11ty.js
+++ src/_includes/layouts/_default/index.11ty.js
@@ -1,48 +1,42 @@
 exports.data = {
   layout: 'layouts/_default/base.11ty.js',
-  pagination: { 
-    data: 'collections.post',
-    size: 3,
-    reverse: true,
-    alias: 'posts'
-  }
 }
 
 exports.render = function (data) {
   return /*html*/ `
-  <main class="p-0">
-    <div class="pt-6 lg:pt-12 lg:flex w-full mr-auto ml-auto">
-      <div class="bg-transparent lg:bg-gradient-to-b from-blue-700 to-black lg:dark:bg-gradient-to-b lg:dark:from-black lg:dark:to-blue-700 lg:w-5/12 px-8 lg:pl-16 lg:pr-16 pt-16 lg:text-white text-center lg:text-right">
-        <h1 class="lg:leading-normal text-3xl lg:text-4xl 2xl:text-6xl lg:pt-10">Home page</h1>
-        <p class="font-sans">This sentence comes from the template.</p>
-        <div class="font-sans">${data.content}</div>
+  <main>
+    <div class="container-home">
+      <div class="homeOpen">
+        <h1>Home page</h1>
+        <p>This sentence comes from the template.</p>
+        <div>${data.content}</div>
       </div>
-      <div class="text-center lg:text-left lg:w-7/12 lg:pl-16 lg:pt-16">
-        <div class="mt-8 border-blue-700 dark:border-white mx-auto my-auto w-3/5 border-solid border-b-4 lg:hidden"></div>
-        <h2 class="h1 mt-4 lg:mt-0 mb-4 lg:mb-10 text-3xl lg:text-5xl 2xl:text-7xl">Latest <a href="/posts/">posts</a></h2>
+      <div class="homePost-Div">
+        <div class="post-line"></div>
+        <h2 class="homePost-Latest">Latest <a href="/posts/">posts</a></h2>
         ${
-          data.pagination.items.map(post => /*html*/ `
+          data.collections.post.slice(0,3).map(post => /*html*/ `
         <div>
-          <h2 class="not-italic tracking-tight text-2xl lg:text-3xl 2xl:text-4xl"><a href="${post.url}">${post.data.title}</a></h2>
-          <p class="font-sans font-bold mt-1 mb-0 leading-5 text-xl lg:text-2xl 2xl:text-3xl">${post.data.subtitle}</p>
-          <p class="font-sans text-sm tracking-normal mt-1 mb-0 dateInfo">
-            <time class="inline" datetime="${this.pub_lastmod(post.date)}}"><strong>${this.pub_lastmod(post.date)}</strong></time>
+          <h2 class="homePost-Title"><a href="${post.url}">${post.data.title}</a></h2>
+          <p class="homePost-Subtitle">${post.data.subtitle}</p>
+          <p class="homePost-Dates">
+            <time datetime="${this.pub_lastmod(post.date)}}"><strong>${this.pub_lastmod(post.date)}</strong></time>
             ${
               post.data.lastmod
               ? /*html*/ `
-            <br />Last modified <time class="inline" datetime="${this.pub_lastmod(post.data.lastmod)}">${this.pub_lastmod(post.data.lastmod)}</time>
+            <br />Last modified <time datetime="${this.pub_lastmod(post.data.lastmod)}">${this.pub_lastmod(post.data.lastmod)}</time>
               `
               : ``
             }
           </p>
-          <p class="font-sans text-base mt-1 mb-4 lg:mb-10">
+          <p class="homePost-Description">
             ${post.data.description}
           </p>
         </div>
           ` 
         ).join('')}
 
-        <p class="font-sans"><a href="/posts/"><strong>All ${data.collections.post.length} posts</strong></a> <span class="text-sm"><em>(listed five per page)</em></span></p>
+        <p><a href="/posts/"><strong>All ${data.collections.post.length} posts</strong></a> <span class="pokey"><em>(listed five per page)</em></span></p>
       </div>
     </div>
   </main>
