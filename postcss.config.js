dd50529a6a046ee01b1c112a0fa241e4f09b8145
Bryce Wray <bwray@wraytx.com>
2021-03-25 08:18:42 -0500

    Initial config after cloning repo

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-preset-env')({ stage: 1 }),
-    require('postcss-clean'),
+    require('autoprefixer'),
   ],
 }
