5338d534dabc9cc8a5524a7a94da6609b247ba5b
Bryce Wray <bwray@wraytx.com>
2021-04-01 14:04:59 -0500

    PurgeCSS and other chgs based on "-njk" repo

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './**/*.njk',
+    './**/*.11ty.js',
+    './**/*.html'
+  ]
+})
+
 module.exports = {
   plugins: [
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ],
 }
